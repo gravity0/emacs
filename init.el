6ee02298b6dfb06a6d0fda71be65787ec603d5c4
greapflute <greapflutea2@yahoo.co.jp>
2015-04-19 03:45:19 +0900

    w3m使えるようにした

--- init.el
+++ init.el
@@ -1,41 +1,17 @@
-;言語を日本語にする
-(set-language-environment 'Japanese)
 ; 極力UTF-8とする
 (prefer-coding-system 'utf-8)
-;emacs-w3mへのパスを通す
-;(require 'w3m-load)
 (add-to-list 'load-path "~/.emacs.d/elisp/color-theme/")
 (require 'color-theme)
 (color-theme-initialize)
 (color-theme-euphoria) 
 
-
-(require 'package)
-(add-to-list 'package-archives
-             '("melpa" . "http://melpa.milkbox.net/packages/") t)
 (package-initialize)
 (unless (package-installed-p 'scala-mode2)
   (package-refresh-contents) (package-install 'scala-mode2))
-
-
-
-(require 'package)
-(add-to-list 'package-archives
-         '("melpa" . "http://melpa.milkbox.net/packages/") t)
-(package-initialize)
-
 (when (not package-archive-contents)
   (package-refresh-contents))
 
-(require 'ensime)
-(add-hook 'scala-mode-hook 'ensime-scala-mode-hook)
-(put 'upcase-region 'disabled nil)
-
 ;;w3m
-(add-to-list 'load-path
-           "~/.emacs.d/elisp/w3m/share/emacs/site-lisp/w3m")
-     (add-to-list 'Info-additional-directory-list
-           "~/.emacs.d/elisp/w3m/share/info")
-     (require 'w3m-load)
-     (setq w3m-use-cookies t) 		 ;ログイン可能にする
-     (setq w3m-favicon-cache-expire-wait nil) ;favicon のキャッシュを消さない
+(add-to-list 'load-path "~/.emacs.d/elisp/w3m/share/emacs/site-lisp/w3m")
+;(add-to-list 'Info-additional-directory-list "~/.emacs.d/elisp/w3m/share/info")
+(require 'w3m-load)
