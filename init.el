040fa426d89fdf70a6e9c94eef881def78b47a3c
greapflute <greapflutea2@yahoo.co.jp>
2015-05-29 03:03:25 +0900

    GNU-Emacsでpathを通した

--- init.el
+++ init.el
@@ -1,3 +1,5 @@
+(setq debug-on-error t)
+
 (require 'package)
 ;;MELPAを追加
 (add-to-list 'package-archives '("melpa" . "http://melpa.milkbox.net/packages/"))
@@ -17,6 +19,10 @@
 ;;~/.emacs.d/elispへロードパスを通す 
 (add-to-list 'load-path "~/.emacs.d/elisp/")
 
+(require 'exec-path-from-shell)
+(when (memq window-system '(mac ns))
+    (exec-path-from-shell-initialize))
+
 ;;init.elを再読み込み
 (global-set-key [f12] 'eval-buffer)
 
@@ -29,15 +35,6 @@
 (custom-set-faces
   '(default ((t (:background "#2d3436" :foreground "#55FF55")
 ))))
-; '(cursor
-;    ((((class color)
-;      (background dark))
-;      (:background "#00AA00"))
-;    (((class color)
-;      (background light))
-;      (:background "#999999"))
-;      (t ())
-;)))
 
 ;;; キーバインド
 (define-key global-map "\C-h" 'delete-backward-char) ; 削除
@@ -94,61 +91,15 @@
 ;;
 (add-to-list 'load-path (concat user-emacs-directory "~/.emacs.d/elisp/ensime"))
 (require 'ensime)
-;(add-to-list 'load-path (concat user-emacs-directory "~/.emacs.d/elisp/scala-mode2"))
-;(require 'scala-mode2)
-;(add-to-list 'load-path (concat user-emacs-directory "~/.emacs.d/s.el/s.el"))
-;(require 's)
+(require 'scala-mode2)
 
-
-;(setenv "PATH" (concat "PATH_TO_SBT:" (getenv "PATH")))
-;(setenv "PATH" (concat "PATH_TO_SCALA:" (getenv "PATH")))
+(setenv "PATH" (concat "PATH_TO_SBT:" (getenv "PATH")))
+(setenv "PATH" (concat "PATH_TO_SCALA:" (getenv "PATH")))
 
 (add-hook 'scala-mode-hook 'ensime-scala-mode-hook)
-;(setq ensime-ac-override-settings nil)
-;ensime内部のauto-complete用の設定
-;(defun my-ac-scala-mode ()
-;  (add-to-list 'ac-sources 'ac-source-dictionary)
-;  (add-to-list 'ac-sources 'ac-source-yasnippet)
-;  (add-to-list 'ac-sources 'ac-source-words-in-buffer)
-;  (add-to-list 'ac-sources 'ac-source-words-in-same-mode-buffers)
-;  (setq ac-sources (reverse ac-sources)) ;;;追記2
-;  )
-
-(add-hook 'ensime-mode-hook 'my-ac-scala-mode)
-
-;;
-;; Auto Complete
-;;
-;;ロードパス
-;(add-to-list 'load-path "~/.emacs.d/elisp/auto-complete")
-;(require 'auto-complete)
-;(require 'auto-complete-config)
-;(add-to-list 'ac-dictionary-directories "~/.emacs.d/elisp/auto-complete/dict")
-;(ac-config-default)
-;(setq ac-use-menu-map t)       ;; 補完メニュー表示時にC-n/C-pで補完候補選択
-;(setq ac-use-fuzzy t)          ;; 曖昧マッチ
-;(ac-set-trigger-key "TAB")
-
 
 ;;w3m
 (add-to-list 'load-path "~/.emacs.d/elisp/w3m/share/emacs/site-lisp/w3m")
 ;(add-to-list 'Info-additional-directory-list "~/.emacs.d/elisp/w3m/share/info")
 (require 'w3m-load)
 (put 'upcase-region 'disabled nil)
-
-;; ensime設定
-;; syntax
-
-(setq ensime-sem-high-faces
-  '(
-   (var . (:foreground "#ff2222"))
-   (val . (:foreground "#dddddd"))
-   (varField . (:foreground "#ff3333"))
-   (valField . (:foreground "#dddddd"))
-   (functionCall . (:foreground "#84BEE3"))
-   (param . (:foreground "#ffffff"))
-   (class . font-lock-type-face)
-   (trait . (:foreground "#084EA8"))
-   (object . (:foreground "#026DF7"))
-   (package . font-lock-preprocessor-face)
-   ))
